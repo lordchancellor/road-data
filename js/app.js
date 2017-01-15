27881e80ec7dcbfb819d84d2c235019303e426fa
Christopher Ford <c.d.j.ford@gmail.com>
2017-01-15 16:53:09 +0000

    Refactored into an API object and added function for pulling individual data items

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
-var roadData = [];
+const roadDataAPI = {
+	roadData: [],
 
-function readRoadData(obj) {
-	for (let item in obj) {
-		if (!isNaN(+obj[item])) {
-			obj[item] = +obj[item];
-		} 
+	readRoadData: function readRoadData(obj) {
+		for (let item in obj) {
+			if (!isNaN(+obj[item])) {
+				obj[item] = +obj[item];
+			} 
+		}
+
+		this.roadData.push(obj);
+	},
+
+	getRoadData: function getRoadData(road) {
+		let individualRoadData = [];
+
+		for (let item of this.roadData) {
+			if (item["Northing"] === road) {
+				individualRoadData.push(item);
+			}
+		}
+
+		return individualRoadData;
 	}
-
-	roadData.push(obj);
 }
-
 d3.csv('../data/devon.csv', (data) => {
 	for (var i = 0; i < data.length; i++) {
-		readRoadData(data[i]);
+		roadDataAPI.readRoadData(data[i]);
 	}
 
 	console.log('Finished reading data');
